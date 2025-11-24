0e2ff7149451b552112d38a6a71883fac16333a6
Rajat Singh Rajawat <rajatsinghrajawatrajawat@gmail.com>
2025-11-24 13:34:34 +0530

    push

--- src/controllers/Accounts.js
+++ src/controllers/Accounts.js
@@ -1,4 +1,4 @@
-const Student = require("../models/StudentModel");
+const Student = require('../models/students');
 const Payment = require("../models/Accounts");
 const { getUniversityByCode } = require('../config/universityConfig');
 
